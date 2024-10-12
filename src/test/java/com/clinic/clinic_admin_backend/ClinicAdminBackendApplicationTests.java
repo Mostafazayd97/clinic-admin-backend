b85d2f973a0c5c8c702fc537b4df13ac91e8f707
MostafaZayed <mostafa.zayed@hypercell.net>
2024-10-12 21:56:27 +0300

    first commit

---
+++ src/test/java/com/clinic/clinic_admin_backend/ClinicAdminBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clinic.clinic_admin_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicAdminBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
